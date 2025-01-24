e4d14b5341c2ab82850378f7c3590b29d981e830
Aryan Babare <babreab17@gmail.com>
2025-01-24 19:42:10 +0530

    added vercel.json

--- frontend/src/components/HomeFeatures.tsx
+++ frontend/src/components/HomeFeatures.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { motion } from 'framer-motion';
 import {  Link, Copy, ChevronRight } from 'lucide-react';
 const HomeFeatures = () => {
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { Link, Copy, ChevronRight, Github } from 'lucide-react';
+import { Copy, } from 'lucide-react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HomeFeatures from '../components/HomeFeatures';
